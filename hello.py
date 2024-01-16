577ff0b975b40731c1e42000d546e638befd9eca
Maria Antonia <mdeazevedomaia@gmail.com>
2024-01-16 10:31:47 -0300

    Primeiro commit

---
+++ hello.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Hello World Multi Línguas
+
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+# Este programa imprime Hello World
+print("Hello, World")
