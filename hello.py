0873d570e2db2f1a361fa169e7c43b406989c8b9
Maria Antonia <mdeazevedomaia@gmail.com>
2024-01-16 11:17:32 -0300

    Implementando o hello world multi línguas

--- hello.py
+++ hello.py
@@ -16,5 +16,20 @@ Execução:
     ou
     ./hello.py
 """
-# Este programa imprime Hello World
-print("Hello, World")
+__version__ = "0.0.1"
+__author__ = "Maria Antônia Maia"
+__license__ = "Unlicense"
+
+import os 
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+
+print(msg)
